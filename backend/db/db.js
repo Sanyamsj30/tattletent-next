844f6a15c98a5e33b16968730208e0d80027436b
yugshah7777 <yugshah7777@gmail.com>
2025-10-02 05:09:42 +0530

    Installing Dependencies

--- backend/db/db.js
+++ backend/db/db.js
@@ -5,11 +5,11 @@ import dotenv from 'dotenv';
 dotenv.config(); // load variables from .env
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 export default pool;
