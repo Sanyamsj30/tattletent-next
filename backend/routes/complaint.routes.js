c5cbea988fedb2644400b9dfd50c0c59cae78775
sanyam jain <sanyamsj30@gmail.com>
2025-10-09 01:43:50 +0530

    Added escalation checking scheduled method

--- backend/routes/complaint.routes.js
+++ backend/routes/complaint.routes.js
@@ -1,12 +1,13 @@
 import express from "express"
 import upload from "../middlewares/upload.middleware.js";
-import { createComplaint, updateComplaint, deleteComplaint ,getComplaints,escalateComplaints} from "../controllers/complaint.controllers.js";
+import { createComplaint, updateComplaintStatus,updateComplaintPriority, deleteComplaint ,getComplaints,escalateComplaints} from "../controllers/complaint.controllers.js";
 
 
 const router = express.Router();
 
 router.post("/",upload.single("photo"),createComplaint);
-router.put("/:id", updateComplaint);
+router.put("/status/:id", updateComplaintStatus);
+router.put("/priority/:id", updateComplaintPriority);
 router.delete("/:id", deleteComplaint);
 router.get('/search', getComplaints);
 router.post("/escalate", escalateComplaints); // manual trigger route
